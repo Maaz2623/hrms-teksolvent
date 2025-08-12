edc8663f4eb50312679109aee2a9f2906451ab13
KalalChandu <kalalshannu19@gmail.com>
2025-08-12 21:32:51 +0530

    Implemented: the sign-up form

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,21 +1,7 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import React from "react";
 
-const SignUpPage = () => {
-  
-  const handleSignUp = () => {
-    authClient.signIn.social({
-      provider: "google",
-    });
-  };
+import { SignUpForm } from "@/modules/auth/ui/components/sign-up-form";
 
-  return (
-    <div>
-      <Button onClick={handleSignUp}>Sign up with Google</Button>
-    </div>
-  );
-};
-
-export default SignUpPage;
+export default function SignUpPage() {
+  return <SignUpForm />;
+}
